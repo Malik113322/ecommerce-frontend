58daee759562d2d83f65866f9382bf54a990f6c7
danish <danishyt96@gmail.com>
2025-08-08 22:58:32 +0530

    add spinner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,6 @@ import { Price } from "../components/Layout/Price";
 import { useNavigate } from "react-router-dom";
 import { useCart } from "../context/cart";
 import toast from "react-hot-toast";
-import Spinner from "../Spinner";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
@@ -18,10 +17,10 @@ const Home = () => {
   const [cart, setCart] = useCart([]);
   const itemsPerPage = 8;
   const [currentPage, setCurrentPage] = useState(1);
-  const start = (currentPage-1)*itemsPerPage;
-  const end = start+itemsPerPage;
-  const paginatedData = products.slice(start,end);
-  const totalPage = Math.ceil(products.length/itemsPerPage);
+  const start = (currentPage - 1) * itemsPerPage;
+  const end = start + itemsPerPage;
+  const paginatedData = products.slice(start, end);
+  const totalPage = Math.ceil(products.length / itemsPerPage);
 
 
   // get products 
@@ -65,18 +64,6 @@ const Home = () => {
     }
   }
 
-  //count product for loadmore 
-  const getCountProduct = async () => {
-    try {
-      setLoading(true)
-      const { data } = await axios.get(`${process.env.REACT_APP_URL}/api/v1/product/product-count`);
-      setLoading(false)
-    } catch (error) {
-      setLoading(false)
-      console.log(error)
-    }
-  }
-
   // get categories 
   const getCategories = async () => {
     try {
@@ -92,8 +79,6 @@ const Home = () => {
 
   useEffect(() => {
     getCategories();
-    getCountProduct();
-    // eslint-disable-next-line 
   }, [])
 
   // select category 
@@ -114,81 +99,84 @@ const Home = () => {
     <Layout title={"eCommerce - A online shop"}>
       <div className="overflow-hidden" >
         {
-          loading? <h2>Loading...</h2> :(<div className="row">
-          <div className="col-md-2 mt-5 ms-5">
-            <h4>Catgories</h4>
-            <div className="d-flex flex-column">
-              {
-                categories.map((c) => (
-                  <Checkbox key={c._id} onChange={(e) => handleFilter(e.target.checked, c._id)}>
-                    {c.name}
-                  </Checkbox>
-                ))
-              }
-            </div>
-            <div className="mt-5">
-              <h2>Price</h2>
-              <Radio.Group onChange={(e) => setRadio(e.target.value)}>
-                {
-                  Price.map((p) => (
-                    <div key={p._id}>
-                      <Radio value={p.array}>
-                        {p.name}
-                      </Radio>
-                    </div>
+          loading ? <div className="w-100 vh-100 d-flex justify-content-center align-items-center">
+            <div class="spinner-border" role="status">
+              <span class="sr-only"></span>
+            </div> </div> : (<div className="row">
+              <div className="col-md-2 mt-5 ms-5">
+                <h4>Catgories</h4>
+                <div className="d-flex flex-column">
+                  {
+                    categories.map((c) => (
+                      <Checkbox key={c._id} onChange={(e) => handleFilter(e.target.checked, c._id)}>
+                        {c.name}
+                      </Checkbox>
+                    ))
+                  }
+                </div>
+                <div className="mt-5">
+                  <h2>Price</h2>
+                  <Radio.Group onChange={(e) => setRadio(e.target.value)}>
+                    {
+                      Price.map((p) => (
+                        <div key={p._id}>
+                          <Radio value={p.array}>
+                            {p.name}
+                          </Radio>
+                        </div>
 
-                  ))
-                }
-              </Radio.Group>
-            </div>
-            <div className="mt-2">
-              <button className="btn btn-danger" onClick={() => window.location.reload()}>REST DEFAULT</button>
-            </div>
-          </div>
-          <div className="col-md-9 text-center ms-5 mt-3 mb-3">
-            <div className="d-flex flex-wrap text-center">
+                      ))
+                    }
+                  </Radio.Group>
+                </div>
+                <div className="mt-2">
+                  <button className="btn btn-danger" onClick={() => window.location.reload()}>REST DEFAULT</button>
+                </div>
+              </div>
+              <div className="col-md-9 text-center ms-5 mt-3 mb-3">
+                <div className="d-flex flex-wrap text-center">
 
-              {
-                paginatedData.map((p) => (
-                  <div key={p._id} className='d-flex flex-wrap w-100' style={{ maxWidth: "300px" }}>
-                    <div className="card d-flex justify-content-between text-center  m-2" style={{ width: '15rem',  height:'22rem'}}>
-                      <div>
-                        <img src={p.image} className="m-auto mt-2" alt="product" style={{ width: "120px", height: "120px" }} />
-                        <div className="card-body" style={{ height: "12rem" }}>
-                          <div className="w-100 h-100">
-                            <p className="card-title">{p.name}</p>
-                          <p className="card-text">{p.description.substring(1,30)}...</p>
-                          <p className="card-title">Price: ${p.price}</p>
+                  {
+                    paginatedData.map((p) => (
+                      <div key={p._id} className='d-flex flex-wrap w-100' style={{ maxWidth: "300px" }}>
+                        <div className="card d-flex justify-content-between text-center  m-2" style={{ width: '15rem', height: '22rem' }}>
+                          <div>
+                            <img src={p.image} className="m-auto mt-2" alt="product" style={{ width: "120px", height: "120px" }} />
+                            <div className="card-body" style={{ height: "12rem" }}>
+                              <div className="w-100 h-100">
+                                <p className="card-title">{p.name}</p>
+                                <p className="card-text">{p.description.substring(1, 30)}...</p>
+                                <p className="card-title">Price: ${p.price}</p>
+                              </div>
+                              <button className="btn btn-success px-1 mx-1" onClick={() => navigate(`/product/${p.slug}`)}>Details</button>
+                              <button className='btn btn-primary px-2' onClick={() => {
+                                setCart([...cart, p]);
+                                localStorage.setItem("cart", JSON.stringify([...cart, p]));
+                                toast.success("Added To Cart Successfully");
+                              }}>Add to Cart</button>
+                            </div>
                           </div>
-                          <button className="btn btn-success px-1 mx-1" onClick={() => navigate(`/product/${p.slug}`)}>Details</button>
-                          <button className='btn btn-primary px-2' onClick={() => {
-                            setCart([...cart, p]);
-                            localStorage.setItem("cart", JSON.stringify([...cart, p]));
-                            toast.success("Added To Cart Successfully");
-                          }}>Add to Cart</button>
                         </div>
                       </div>
-                    </div>
-                  </div>
-                ))
-              }
-            </div>
-          <div className="paginationCard w-100 d-flex flex-wrap justify-content-center align-items-center">
-            <button className="btn btn-danger" 
-            disabled={currentPage===totalPage}
-            onClick={()=>currentPage<totalPage?setCurrentPage(pre=>pre+1):''}>Next</button>
-            {Array.from({length:totalPage}, (_, index)=>(
-              <button onClick={()=>setCurrentPage(index+1)} className="btn btn-outline-danger border-2  mx-1 text-black">{index+1}</button>
-            ))}
-            <button className="btn btn-danger"
-            onClick={()=>currentPage>1?setCurrentPage(pre=>pre-1):''}
-            >Prev</button>
-          </div>
-          </div>
-        </div>)
+                    ))
+                  }
+                </div>
+                <div className="paginationCard w-100 d-flex flex-wrap justify-content-center align-items-center">
+                  <button className="btn btn-danger"
+                    disabled={currentPage === totalPage}
+                    onClick={() => currentPage < totalPage ? setCurrentPage(pre => pre + 1) : ''}>Next</button>
+                  {Array.from({ length: totalPage }, (_, index) => (
+                    <button onClick={() => setCurrentPage(index + 1)} className="btn btn-outline-danger border-2  mx-1 text-black">{index + 1}</button>
+                  ))}
+                  <button className="btn btn-danger"
+                    onClick={() => currentPage > 1 ? setCurrentPage(pre => pre - 1) : ''}
+                  >Prev</button>
+                </div>
+              </div>
+            </div>)
         }
       </div>
-  
+
     </Layout>
   );
 };
