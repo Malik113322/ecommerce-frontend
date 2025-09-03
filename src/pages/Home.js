aded3e198f1c6e1134926ae84f6e145b0121a1f3
Mohd Danish Malik <121788489+Malik113322@users.noreply.github.com>
2025-09-04 01:34:22 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -68,7 +68,7 @@ const Home = () => {
       const { data } = await axios.get(
         `${process.env.REACT_APP_URL}/api/v1/category/categories`
       );
-      if (data.success) setCategories(data.category);
+      if (data.success) setCategories(data.categories);
     } catch (error) {
       console.log(error);
     }
